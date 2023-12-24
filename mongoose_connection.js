4ce04af327848fa3dc037f6834dc3da54bc0aa9d
NeerajaNeelima <023neeraja@gmail.com>
2023-12-24 14:50:35 +0530

    successfully

--- mongoose_connection.js
+++ mongoose_connection.js
@@ -2,7 +2,7 @@ const mongoose=require("mongoose")
 
 require('dotenv').config()
 const con_url=process.env.MONGOBD;
-const DB='mongodb+srv://023neeraja:neelima143@cluster0.d5fmcua.mongodb.net/Starup_companies?retryWrites=true&w=majority'
+//const DB='mongodb+srv://023neeraja:neelima143@cluster0.d5fmcua.mongodb.net/Starup_companies?retryWrites=true&w=majority'
 mongoose.connect(con_url,{
     useNewUrlParser: "true",
 useUnifiedTopology: "true"})
